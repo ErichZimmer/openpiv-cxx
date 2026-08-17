4836b2def8717aba024291ed4fe21378262d604d
ErichZimmer <Erich_Zimmer@hotmail.com>
2026-08-16 21:42:42 -0500

    DLL export issues with clang and intel icc

--- openpiv/core/dll_export.h
+++ openpiv/core/dll_export.h
@@ -1,12 +1,17 @@
-
 #pragma once
 
-#ifndef DLL_EXPORT
+#if defined(EXPORT_DLL_SYMBOLS)
 
-#if defined(_MSC_VER) && defined(EXPORT_DLL_SYMBOLS)
-#define DLL_EXPORT __declspec(dllexport)
+    #if defined(_WIN32) || defined(__CYGWIN__)
+        #define DLL_EXPORT __declspec(dllexport)
+    #elif defined(__GNUC__) || defined(__clang__)
+        #define DLL_EXPORT __attribute__((visibility("default")))
+    #else
+        #define DLL_EXPORT
+    #endif
+
 #else
-#define DLL_EXPORT
-#endif
+
+    #define DLL_EXPORT
 
 #endif
